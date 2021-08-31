26871c2a62bede575356a19c36da5e8a74f94da1
pabloapausa <pabloapausa@outlook.com>
2021-08-31 23:37:44 +0200

    Test author controller

--- project/lib/controllers/authorController.test.ts
+++ project/lib/controllers/authorController.test.ts
@@ -1,11 +1,11 @@
 import Author from '../models/authorModel';
 import handle from '../../utils/error';
 import {
-  postAuthor,
+  postAuthor, getAuthor, putAuthor, deleteAuthor,
 } from './authorController';
 
-jest.mock('../models/authorModel');
 jest.mock('../../utils/error');
+jest.mock('../models/authorModel');
 
 describe('Given a postAuthor function', () => {
   describe('When is invoked', () => {
@@ -18,16 +18,108 @@ describe('Given a postAuthor function', () => {
     };
 
     describe('And resolves', () => {
-      test('Then "res.send" should be called', async () => {
-        (Author.create as jest.Mock).mockResolvedValue({});
+      test('Then "res.send" is called', async () => {
+        (Author.create as jest.Mock)
+          .mockResolvedValue({});
         await postAuthor(req, res);
+
         expect(res.send).toHaveBeenCalled();
       });
     });
     describe('And rejectes', () => {
-      test('Then "handle" should be called', async () => {
-        (Author.create as jest.Mock).mockRejectedValue({});
+      test('Then "handle" is called', async () => {
+        (Author.create as jest.Mock)
+          .mockRejectedValue({});
         await postAuthor(req, res);
+
+        expect(handle).toHaveBeenCalled();
+      });
+    });
+  });
+});
+
+describe('Given a getAuthor function', () => {
+  describe('When is invoked', () => {
+    const req: any = { query: { authorId: '' } };
+    const res: any = {
+      status: jest.fn(),
+      send: jest.fn(),
+    };
+
+    describe('And resolves', () => {
+      test('Then "res.send" is called', async () => {
+        (Author.findById as jest.Mock)
+          .mockResolvedValue({});
+        await getAuthor(req, res);
+
+        expect(res.send).toHaveBeenCalled();
+      });
+    });
+    describe('And rejectes', () => {
+      test('Then "handle" is called', async () => {
+        (Author.findById as jest.Mock)
+          .mockRejectedValue({});
+        await getAuthor(req, res);
+
+        expect(handle).toHaveBeenCalled();
+      });
+    });
+  });
+});
+
+describe('Given a "putAuthor" function', () => {
+  describe('When is invoked', () => {
+    const req: any = {
+      query: { authorId: '' },
+      body: {},
+    };
+    const res: any = {
+      status: jest.fn(),
+      send: jest.fn(),
+    };
+    describe('And resolves', () => {
+      test('Then "res.send" is called', async () => {
+        (Author.findByIdAndUpdate as jest.Mock)
+          .mockResolvedValue({});
+        await putAuthor(req, res);
+
+        expect(res.send).toHaveBeenCalled();
+      });
+    });
+    describe('And rejectes', () => {
+      test('Then "handle" is called', async () => {
+        (Author.findByIdAndUpdate as jest.Mock)
+          .mockRejectedValue({});
+        await putAuthor(req, res);
+
+        expect(handle).toHaveBeenCalled();
+      });
+    });
+  });
+});
+
+describe('Given a "deleteAuthor" function', () => {
+  describe('When is invoked', () => {
+    const req: any = { query: { authorId: '' } };
+    const res: any = {
+      status: jest.fn(),
+      send: jest.fn(),
+    };
+    describe('And resolves', () => {
+      test('Then "res.send" is called', async () => {
+        (Author.findByIdAndDelete as jest.Mock)
+          .mockResolvedValue({});
+        await deleteAuthor(req, res);
+
+        expect(res.send).toHaveBeenCalled();
+      });
+    });
+    describe('And rejectes', () => {
+      test('Then "handle" is called', async () => {
+        (Author.findByIdAndDelete as jest.Mock)
+          .mockRejectedValue({});
+        await deleteAuthor(req, res);
+
         expect(handle).toHaveBeenCalled();
       });
     });
