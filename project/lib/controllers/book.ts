462e401aaa5b717bbd4f4f230c3e9396c423062e
pabloapausa <pabloapausa@outlook.com>
2021-08-31 12:20:27 +0200

    Add comments on book controller

--- project/lib/controllers/book.ts
+++ project/lib/controllers/book.ts
@@ -1,31 +1,34 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
 import Book from '../models/book';
 import handle from '../../utils/error';
 
-export const getBooks = async (req: any, res: any) => {
+export const postBook = async ( // Creates a book.
+  req: nextApiRequest, res: NextApiResponse,
+) => {
   try {
     res.status(200);
   } catch (error) { handle.call(this, res, error); }
 };
 
-export const getBook = async (req: any, res: any) => {
+export const getBook = async ( // Retrieves a book.
+  req: nextApiRequest, res: NextApiResponse,
+) => {
   try {
     res.status(200);
   } catch (error) { handle.call(this, res, error); }
 };
 
-export const postBook = async (req: any, res: any) => {
+export const putBook = async ( // Updates a book.
+  req: nextApiRequest, res: NextApiResponse,
+) => {
   try {
     res.status(200);
   } catch (error) { handle.call(this, res, error); }
 };
 
-export const putBook = async (req: any, res: any) => {
-  try {
-    res.status(200);
-  } catch (error) { handle.call(this, res, error); }
-};
-
-export const deleteBook = async (req: any, res: any) => {
+export const deleteBook = async ( // Deletes a book.
+  req: nextApiRequest, res: NextApiResponse,
+) => {
   try {
     res.status(200);
   } catch (error) { handle.call(this, res, error); }
--- project/pages/api/books/index.ts
+++ project/pages/api/books/index.ts
@@ -1,9 +1,8 @@
 import connect from '../../../lib/configure/database';
 import request from '../../../utils/methods';
-import { getBooks, postBook } from '../../../lib/controllers/book';
+import { postBook } from '../../../lib/controllers/book';
 
 const handler = async (req: any, res: any) => {
-  if (req.method === request.GET) await getBooks(req, res);
   if (req.method === request.POST) await postBook(req, res);
 };
 
