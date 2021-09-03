a2998f2a21aa4ad335adf74fcdf900f55121ec12
pabloapausa <pabloapausa@outlook.com>
2021-09-03 18:11:49 +0200

    Secure dashboard page

--- project/pages/[userId]/index.tsx
+++ project/pages/[userId]/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useSession } from 'next-auth/client';
 
 export default function User() {
+  const [session, loading] = useSession();
   return <h1>User</h1>;
 }
