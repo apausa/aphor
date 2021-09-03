0dc252f7af9f533aed80639580a4fe9a9f2b81e8
pabloapausa <pabloapausa@outlook.com>
2021-09-03 19:03:36 +0200

    Simplify Profile page

--- project/pages/[userId]/index.tsx
+++ project/pages/[userId]/index.tsx
@@ -1,19 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import { useSession, signIn } from 'next-auth/client';
 
 export default function User() {
-  // Sets loading initial state.
   const [session, loading] = useSession();
-  const secure = async () => {
-    // Returns a promise with an user object.
-    const session = await getSession();
-    // Sets loading state to false.
-    if (session) return setLoading(false);
-    return signIn();
-  };
-
-  // Runs once for every page reload.
-  useEffect(() => { secure(); }, []);
-  if (loading) return <h1>Loading</h1>;
+  if (!session) return signIn();
   return <h1>Profile</h1>;
 }
