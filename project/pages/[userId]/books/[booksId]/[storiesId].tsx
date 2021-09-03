3d6eb8edd6ca30be18a886ff385568e44ac814fb
pabloapausa <pabloapausa@outlook.com>
2021-09-03 18:40:27 +0200

    Secure Stories page

--- project/pages/[userId]/books/[booksId]/[storiesId].tsx
+++ project/pages/[userId]/books/[booksId]/[storiesId].tsx
@@ -1,7 +1,15 @@
-import React from 'react';
-import { useSession } from 'next-auth/client';
+import React, { useEffect, useState } from 'react';
+import { getSession, signIn } from 'next-auth/client';
 
 export default function Stories() {
-  const [session, loading] = useSession();
+  const [loading, setLoading] = useState(true);
+  const secure = async () => {
+    const session = await getSession();
+    if (session) return setLoading(false);
+    return signIn;
+  };
+
+  useEffect(() => { secure(); }, []);
+  if (loading) return <h1>Loading</h1>;
   return <h1>Stories</h1>;
 }
