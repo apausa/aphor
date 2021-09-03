7ad877a98f83144753afbbe715359b38b0dd52e1
pabloapausa <pabloapausa@outlook.com>
2021-09-03 20:06:16 +0200

    Handle session absence on 'getServerSideProps'

--- project/pages/[userId]/about.tsx
+++ project/pages/[userId]/about.tsx
@@ -1,18 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import { useSession, getSession, signIn } from 'next-auth/client';
 
-// Client side page secured.
 export default function About() {
-  const [session, loading] = useSession();
+  const [session] = useSession();
 
-  if (loading) return null;
-  if (!session) return signIn();
   return <h1>About</h1>;
 }
 
-// Server side page secured.
+// Client and server side page secured.
 export async function getServerSideProps(context: any) {
   const session = await getSession(context);
 
-  return { props: { session } };
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/api/auth/signin?callbackUrl=http://localhost:3000/',
+      },
+    };
+  }
+  return {
+    props: {
+      session,
+    },
+  };
 }
