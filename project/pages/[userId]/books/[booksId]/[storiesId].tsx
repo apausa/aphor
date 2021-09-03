cc3cea60cc0651d2823886e74aa64fcc8118439b
pabloapausa <pabloapausa@outlook.com>
2021-09-03 18:41:07 +0200

    Add some comments

--- project/pages/[userId]/books/[booksId]/[storiesId].tsx
+++ project/pages/[userId]/books/[booksId]/[storiesId].tsx
@@ -2,13 +2,17 @@ import React, { useEffect, useState } from 'react';
 import { getSession, signIn } from 'next-auth/client';
 
 export default function Stories() {
+  // Sets loading initial state to true.
   const [loading, setLoading] = useState(true);
   const secure = async () => {
+    // Returns a promise with an user object.
     const session = await getSession();
+    // Sets loading state to false.
     if (session) return setLoading(false);
     return signIn;
   };
 
+  // Runs once for every page reload.
   useEffect(() => { secure(); }, []);
   if (loading) return <h1>Loading</h1>;
   return <h1>Stories</h1>;
