b78a4b91900e422f12626dea45e886fb5256d7fe
pabloapausa <pabloapausa@outlook.com>
2021-09-04 20:10:01 +0200

    Create basic header

--- project/pages/_app.tsx
+++ project/pages/_app.tsx
@@ -3,13 +3,15 @@ import type { AppProps } from 'next/app';
 import { Provider } from 'next-auth/client';
 import React from 'react';
 import Head from 'next/head';
+import Header from '../components/header';
+import '../styles/layout.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider session={pageProps.session}>
       <Head>
         <title>Project</title>
-        <meta name="description" content="Description" />
+        <meta name="descr iption" content="Description" />
         <meta property="og:title" content="Project" />
         <meta property="og:description" content="Description" />
         <meta property="og:image" content="" />
@@ -17,6 +19,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta property="og:url" content="http://localhost:3000/" />
         <link rel="icon" href="" />
       </Head>
+      <Header />
       <Component {...pageProps} />
     </Provider>
   );
