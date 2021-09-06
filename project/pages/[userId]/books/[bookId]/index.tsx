6a5a576679bca0fcba8567d53aa9a2db6da17335
pabloapausa <pabloapausa@outlook.com>
2021-09-06 16:24:32 +0200

    Refactor [bookId] page

--- project/pages/[userId]/books/[bookId]/index.tsx
+++ project/pages/[userId]/books/[bookId]/index.tsx
@@ -4,14 +4,18 @@ import axios from 'axios';
 import Link from 'next/link';
 
 export default function Book({ data, userId, bookId }: any) {
+  const { books, name, image } = data;
+  const { stories } = books.filter((book: any) => book._id === bookId);
   return (
     <>
       <h1>User, book</h1>
       <ul>
-        {data.stories.map((story: any) => (
+        {stories.map((story: any) => (
           <Link href={`/${userId}/books/${bookId}/${story._id}`}>
             <li>
               <ul>
+                <li>{name}</li>
+                <li>{image}</li>
                 <li>{story.title}</li>
                 <li>{story.date}</li>
                 <li>{story.body}</li>
@@ -27,6 +31,6 @@ export default function Book({ data, userId, bookId }: any) {
 export async function getServerSideProps(context: any) {
   const { params: { userId, bookId } } = context;
   const { data } = await axios
-    .get(`http://localhost:3000/api/book/${bookId}`);
+    .get(`http://localhost:3000/api/user/${userId}`);
   return { props: { userId, bookId, data } };
 }
