838ce933e815c3e5c0b68bf2a809afa794c8cd7c
pabloapausa <pabloapausa@outlook.com>
2021-09-08 15:42:43 +0200

    Push staged changes

--- project/pages/api/user/[userId].ts
+++ project/pages/api/user/[userId].ts
@@ -21,7 +21,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
           },
         })
         .populate({
-          path: 'authors',
+          path: 'library',
           select: ['_id', 'image', 'title', 'date'],
         });
       res.status(200);
