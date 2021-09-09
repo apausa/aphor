f7f7a173a6e148672760800777d3f544ec3a536a
pabloapausa <pabloapausa@outlook.com>
2021-09-09 13:06:35 +0200

    Push previous commit

--- project/pages/api/user/[userId].ts
+++ project/pages/api/user/[userId].ts
@@ -21,7 +21,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         })
         .populate({
           path: 'authors',
-          select: ['_id', 'name', 'image', 'books'],
+          select: ['_id', 'name', 'image'],
         });
       res.status(200);
       res.send(user);
--- project/pages/index.tsx
+++ project/pages/index.tsx
@@ -16,10 +16,22 @@ export async function getServerSideProps(context: any) {
   // Retrieve followed authors.
   const { data: { authors } } = await axios
     .get(`http://localhost:3000/api/user/${id}`);
-  console.log(authors);
+  const promises = authors.map(({ _id }: any) => axios
+    .get(`http://localhost:3000/api/user/${_id}`));
+  const keep: any = await Promise.allSettled(promises);
+  const users = keep.map(({
+    value: {
+      data: {
+        _id, image, books, name,
+      },
+    },
+  }: any) => ({
+    _id, image, books, name,
+  }));
   return {
     props: {
       session,
+      users,
     },
   };
 }
