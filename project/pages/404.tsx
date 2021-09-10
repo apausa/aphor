73519b5b44a6819e56de2bacdafedb97c84568db
pabloapausa <pabloapausa@outlook.com>
2021-09-10 12:40:34 +0200

    Fix some styling

--- project/pages/404.tsx
+++ project/pages/404.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from '../styles/404.module.scss';
 
 export default function Custom404() {
   return (
     <main>
-      <h1>404, page not found...</h1>
+      <h1 className={styles.title}>I&apos;m very sorry...</h1>
     </main>
   );
 }
