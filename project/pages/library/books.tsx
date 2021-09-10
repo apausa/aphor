31645fe074872ddecf59bfd75f6a74c0ddca85f1
pabloapausa <pabloapausa@outlook.com>
2021-09-10 13:00:10 +0200

    Change font size

--- project/pages/library/books.tsx
+++ project/pages/library/books.tsx
@@ -20,17 +20,15 @@ export default function Books({ users }: any) {
                   <li>
                     <ul className={styles.first__information}>
                       <li><Image className={styles.information__image} src={user.image} width="18" height="18" /></li>
-                      <Link href={`/${user._id}/books/${book._id}`}>
-                        <li className={styles.information__book}>
-                          {book.title}
-                          {' '}
-                          /
-                        </li>
-                      </Link>
                       <Link href={`/${user._id}`}>
                         <li className={styles.information__name}>
                           {user.fullName}
                           .
+                        </li>
+                      </Link>
+                      <Link href={`/${user._id}/books/${book._id}`}>
+                        <li className={styles.information__book}>
+                          {book.title}
                         </li>
                       </Link>
                     </ul>
